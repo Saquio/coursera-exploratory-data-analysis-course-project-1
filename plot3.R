c8bbcf35d6ac10245613d13db5db829906987f20
Saquio <47035174+Saquio@users.noreply.github.com>
2019-03-31 14:45:02 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,30 +1,21 @@
-# Coursera Data Science: Exploratory Data Analysis
-# Course project 1: plot3
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-## load the data
-rm(list = ls())
-data <- read.table("household_power_consumption.txt", header = T, 
-                   sep = ";", na.strings = "?")
-# convert the date variable to Date class
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
-# Subset the data
-data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-# Convert dates and times
-data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+# calling the basic plot functions
+plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-# Plot 3
-data$datetime <- as.POSIXct(data$datetime)
-
-attach(data)
-plot(Sub_metering_1 ~ datetime, type = "l", 
-     ylab = "Energy sub metering", xlab = "")
-lines(Sub_metering_2 ~ datetime, col = "Red")
-lines(Sub_metering_3 ~ datetime, col = "Blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-dev.copy(png, file = "plot3.png", height = 480, width = 480)
-dev.off()
-detach(data)
+# annotating graph
+title(main="Energy sub-metering")
