cdeadfa673037cd018312278f85cc816c1e0b35f
Saquio <47035174+Saquio@users.noreply.github.com>
2019-03-31 15:34:10 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,19 @@
-#Reading, naming and subsetting power consumption data
-power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+# download and unzip data
+setwd('E:\\Dropbox\\coursera\\Exploratory Data Analysis')
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip('./data/household_power_consumption.zip', exdir = './data')
 
-#calling the basic plot function
-hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+# read data into R
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
 
-# annotating graph
-title(main="Global Active Power")
+# Plot 1
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot1.png', width = 480, height = 480, units='px')
+# plot figure
+with(data, hist(Global_active_power, xlab = 'Global Active Power (kilowatt)', main = 'Global Active Power', col = 'red'))
+# close device
+dev.off()
