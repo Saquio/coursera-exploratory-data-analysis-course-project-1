76fde98dcf62f6c6e8485ddefa8f7657b3b6af19
Saquio <47035174+Saquio@users.noreply.github.com>
2019-03-31 14:44:18 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,24 +1,17 @@
-# Coursera Data Science: Exploratory Data Analysis
-# Course project 1: plot2
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-# load the data
-rm(list = ls())
-data <- read.table("household_power_consumption.txt", header = T, 
-                   sep = ";", na.strings = "?")
-# convert the date variable to Date class
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
-# Subset the data
-data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-# Convert dates and times
-data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+# calling the basic plot function
+plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 
-# Plot 2
-data$datetime <- as.POSIXct(data$datetime)
-attach(data)
-plot(Global_active_power ~ datetime, type = "l",
-     ylab = "Global Active Power (kilowatts)", xlab = "")
-dev.copy(png, file = "plot2.png", height = 480, width = 480)
-dev.off()
-detach(data)
+# annotating graph
+title(main="Global Active Power Vs Time")
